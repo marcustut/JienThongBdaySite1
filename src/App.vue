2a19243c7418bcaff486d0d7cbc0f9aaeb0b72d9
Marcus Lee <marcustutorial@hotmail.com>
2020-10-28 20:06:47 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <HelloWorld msg="Welcome to your Gift Redeem Counter"/>
+    <div class="pin-box">
+      <input v-model="num1" class="box" type="text" maxlength="1"/>
+      <input v-model="num2" class="box" type="text" maxlength="1"/>
+      <input v-model="num3" class="box" type="text" maxlength="1"/>
+      <input v-model="num4" class="box" type="text" maxlength="1"/>
+    </div>
+    <p>{{ message }}</p>
+    <button @click="checkPIN">Submit</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      num1: "",
+      num2: "",
+      num3: "",
+      num4: "",
+      message: "",
+    }
+  },
+  methods: {
+    checkPIN() {
+      if (this.num1 == "7" && this.num2 == "2" && this.num3 == "4" && this.num4 == "5") {
+        this.message = "Congratulations! You can go to the next one!"
+      }
+      else {
+        this.message = "INCORRECT! Try again."
+      }
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: darkslategrey;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+
+.pin-box {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: auto;
+  width: 25%;
+  padding: 10px;
+}
+
+.box {
+  height: 75px;
+  width: 50px;
+  font-size: 50px;
+  text-align: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      <a href="https://www.youtube.com/watch?v=c6PGKV0C-Ok" target="_blank" rel="noopener">click me for motivation</a>.
+    </p>
+    <h3>Enter the PIN Below</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
